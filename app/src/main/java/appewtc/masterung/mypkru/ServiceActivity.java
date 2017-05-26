d899cc3b14137e44b5bfd8b141a870c2e78f907d
masterUNG <phrombutr@gmail.com>
2017-05-26 13:37:16 +0700

    Get Location OK

--- app/src/main/java/appewtc/masterung/mypkru/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/mypkru/ServiceActivity.java
@@ -1,5 +1,12 @@
 package appewtc.masterung.mypkru;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +21,10 @@ public class ServiceActivity extends AppCompatActivity {
     private TextView textView;
     private ListView listView;
     private String[] loginStrings;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private double latADouble = 7.909936, lngADouble = 98.388240;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,84 @@ public class ServiceActivity extends AppCompatActivity {
 
 
     }   // Main Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (networkLocation != null) {
+            latADouble = networkLocation.getLatitude();
+            lngADouble = networkLocation.getLongitude();
+        }
+
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+            latADouble = gpsLocation.getLatitude();
+            lngADouble = gpsLocation.getLongitude();
+        }
+
+        Log.d("26MayV2", "Lat ==> " + latADouble);
+        Log.d("26MayV2", "Lng ==> " + lngADouble);
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        locationManager.removeUpdates(locationListener);
+
+    }
+
+    public Location myFindLocation(String strProvider) {
+
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
+
+        }
+
+        return location;
+    }
+
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            latADouble = location.getLatitude();
+            lngADouble = location.getLongitude();
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
 
     private void showName() {
 
@@ -77,6 +166,14 @@ public class ServiceActivity extends AppCompatActivity {
     private void initialView() {
         textView = (TextView) findViewById(R.id.txtNameLogin);
         listView = (ListView) findViewById(R.id.livFriend);
+
+        //Setup
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
     }
 
 }   // Main Class
