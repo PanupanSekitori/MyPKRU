71c889cb892d63cf014a1cae1e6ea1c9dbc051ff
masterUNG <phrombutr@gmail.com>
2017-05-26 11:39:55 +0700

    ListView OK

--- app/src/main/java/appewtc/masterung/mypkru/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/mypkru/ServiceActivity.java
@@ -2,8 +2,12 @@ package appewtc.masterung.mypkru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -38,6 +42,35 @@ public class ServiceActivity extends AppCompatActivity {
     }
 
     private void createListView() {
+
+        try {
+
+            String urlJSON = "http://swiftcodingthai.com/pkru/GetUserMaster.php";
+            GetAllData getAllData = new GetAllData(this);
+            getAllData.execute(urlJSON);
+
+            String strJSON = getAllData.get();
+            Log.d("26MayV1", "JSON ==> " + strJSON);
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+            String[] nameStrings = new String[jsonArray.length()];
+            String[] imageStrings = new String[jsonArray.length()];
+
+            for (int i=0;i<jsonArray.length();i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                nameStrings[i] = jsonObject.getString("Name");
+                imageStrings[i] = jsonObject.getString("Image");
+                Log.d("26MayV1", "Result (" + i + ") ==> " + nameStrings[i] + " : " + imageStrings[i]);
+
+            }   // for
+
+            FriendAdapter friendAdapter = new FriendAdapter(this, nameStrings, imageStrings);
+            listView.setAdapter(friendAdapter);
+
+        } catch (Exception e) {
+            Log.d("26MayV1", "e createListView ==> " + e.toString());
+        }
 
     }
 
